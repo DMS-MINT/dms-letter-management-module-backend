054c13eeb5d47149a7f92aea8ec78543685c12d2
Yab1 <yabilisanu@gmail.com>
2024-05-22 16:30:58 +0300

    feat: Implemented ParticipantListApi with OutputSerializer

--- core/participants/apis.py
+++ core/participants/apis.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.views import APIView
 
-# Create your views here.
+from core.users.apis import UserListApi
+
+from .models import Participant
+
+
+class ParticipantListApi(APIView):
+    class OutputSerializer(serializers.HyperlinkedModelSerializer):
+        role = serializers.ChoiceField(choices=Participant.Roles.choices, source="get_role_display")
+        user = UserListApi.OutputSerializer()
+
+        class Meta:
+            model = Participant
+            fields: list[str] = [
+                "id",
+                "role",
+                "user",
+                "message",
+                "created",
+                "modified",
+            ]
