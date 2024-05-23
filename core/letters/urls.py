20dbfa3ae7f65adf29c272c733869fcd96ee2fb1
Yab1 <yabilisanu@gmail.com>
2024-05-23 06:57:43 +0300

    feat: Added URL patterns for LetterCreateApi

--- core/letters/urls.py
+++ core/letters/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .apis import LetterDetailApi, LetterListApi
+from .apis import LetterCreateApi, LetterDetailApi, LetterListApi
 
 app_name = "letter"
 
 urlpatterns: list[URLPattern] = [
     path("", LetterListApi.as_view(), name="letter-list"),
     path("<uuid:letter_id>/", LetterDetailApi.as_view(), name="letter-detail"),
+    path("create/", LetterCreateApi.as_view(), name="letter-create"),
 ]
