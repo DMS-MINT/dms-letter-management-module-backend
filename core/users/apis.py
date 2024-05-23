82d96783455770948d212788d3704e1a40c34016
Yab1 <yabilisanu@gmail.com>
2024-05-23 10:42:12 +0300

    refactor: Moved serializers for Guest, Member, User creation, and detail to users.serializers

--- core/users/apis.py
+++ core/users/apis.py
@@ -1,4 +1,3 @@
-from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_polymorphic.serializers import PolymorphicSerializer
@@ -6,16 +5,7 @@ from rest_polymorphic.serializers import PolymorphicSerializer
 from core.common.utils import get_list, get_object
 from core.users.models import BaseUser, Guest, Member
 
-
-class GustListSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    name = serializers.CharField()
-
-
-class MemberListSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    full_name = serializers.CharField()
-    job_title = serializers.CharField()
+from .serializers import GuestDetailSerializer, GustListSerializer, MemberDetailSerializer, MemberListSerializer
 
 
 class UserListApi(APIView):
@@ -38,28 +28,6 @@ class UserListApi(APIView):
         serializer = self.OutputSerializer(users, many=True)
 
         return Response(data=serializer.data)
-
-
-class GuestDetailSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    name = serializers.CharField()
-    email = serializers.CharField()
-    phone_number = serializers.CharField()
-    address = serializers.CharField()
-    postal_code = serializers.CharField()
-
-
-class MemberDetailSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    username = serializers.CharField()
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    full_name = serializers.CharField()
-    job_title = serializers.CharField()
-    department = serializers.CharField()
-    email = serializers.EmailField()
-    phone_number = serializers.CharField()
-    modified = serializers.DateTimeField()
 
 
 class UserDetailAPI(APIView):
