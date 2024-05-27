1d735cf2fdebd13e745e1b17e74f4dadc7472c32
Yab1 <yabilisanu@gmail.com>
2024-05-27 22:13:55 +0300

    refactor: Changed the root page to be api docs

--- config/urls.py
+++ config/urls.py
@@ -5,14 +5,10 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
     path("admin/", admin.site.urls),
-    path("api/", include(("core.api.urls", "api"))),
+    path("api/", include(("core.api.urls", "api"), namespace="api")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/docs/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="docs",
-    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from config.settings.debug_toolbar.setup import DebugToolbarSetup  # noqa
