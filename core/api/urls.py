bfd62bc171914d7286b9d702f0c08b975c9bf993
Yab1 <yabilisanu@gmail.com>
2024-05-27 22:22:49 +0300

    refactor: Removed core.participants.urls

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,8 +4,4 @@ from django.urls.resolvers import URLResolver
 urlpatterns: list[URLResolver] = [
     path("letters/", include(("core.letters.urls", "letters"), namespace="letters")),
     path("users/", include(("core.users.urls", "users"), namespace="users")),
-    path(
-        "participants/",
-        include(("core.participants.urls", "participants"), namespace="participants"),
-    ),
 ]
