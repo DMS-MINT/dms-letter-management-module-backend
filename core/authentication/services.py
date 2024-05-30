dd64849b199f2987685262d69d5adaf18843db70
Yab1 <yabilisanu@gmail.com>
2024-05-30 11:01:08 +0300

    feat: Implement JWT authentication services

---
+++ core/authentication/services.py
@@ -0,0 +1,20 @@
+import uuid
+
+from core.users.models import Member
+
+
+def auth_user_get_jwt_secret_key(user: Member) -> str:
+    return str(user.jwt_key)
+
+
+def auth_jwt_response_payload_handler(token, user=None, request=None, issued_at=None):
+    return {
+        "token": token,
+    }
+
+
+def auth_logout(user: Member) -> Member:
+    user.jwt_key = uuid.uuid4()
+    user.save(update_fields=["jwt_key"])
+
+    return user
