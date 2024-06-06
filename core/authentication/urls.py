5d8cfbcc1a552ab8636c4fd206b39d30f3956338
Yab1 <yabilisanu@gmail.com>
2024-06-06 18:45:56 +0300

    refactor: Renamed api

--- core/authentication/urls.py
+++ core/authentication/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .apis import SecureAPIView, UserJwtLoginApi, UserJwtLogoutApi, UserMeApi
+from .apis import LoginApi, LogoutApi, UserMeApi
 
 app_name = "authentication"
 
 urlpatterns: list[URLPattern] = [
-    path("login/", UserJwtLoginApi.as_view(), name="auth-login"),
-    path("logout/", UserJwtLogoutApi.as_view(), name="auth-logout"),
+    path("login/", LoginApi.as_view(), name="auth-login"),
+    path("logout/", LogoutApi.as_view(), name="auth-logout"),
     path("me/", UserMeApi.as_view(), name="auth-me"),
-    path("secure/", SecureAPIView.as_view(), name="auth-secure"),
 ]
