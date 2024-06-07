f74dde705cc86aee66164b6fe8bc348bb837d80a
Yab1 <yabilisanu@gmail.com>
2024-06-07 16:35:06 +0300

    refactor: Deprecate /me endpoint

--- core/authentication/urls.py
+++ core/authentication/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .apis import LoginApi, LogoutApi, UserMeApi
+from .apis import LoginApi, LogoutApi
 
 app_name = "authentication"
 
 urlpatterns: list[URLPattern] = [
     path("login/", LoginApi.as_view(), name="auth-login"),
     path("logout/", LogoutApi.as_view(), name="auth-logout"),
-    path("me/", UserMeApi.as_view(), name="auth-me"),
 ]
