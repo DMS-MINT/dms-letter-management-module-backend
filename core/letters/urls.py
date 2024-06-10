14baad322fe6728d11d7f392af5ddfb606898bbb
Yab1 <yabilisanu@gmail.com>
2024-06-10 13:55:28 +0300

    feat: Implemented letter forwarding endpoint

--- core/letters/urls.py
+++ core/letters/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .apis import DeleteLetterApi, LetterCreateApi, LetterDetailApi, LetterListApi, LetterUpdateApi
+from .apis import (
+    DeleteLetterApi,
+    LetterApproveApi,
+    LetterCreateApi,
+    LetterDetailApi,
+    LetterForwardApi,
+    LetterListApi,
+    LetterUpdateApi,
+)
 
 app_name = "letters"
 
@@ -11,4 +19,6 @@ urlpatterns: list[URLPattern] = [
     path("create/", LetterCreateApi.as_view(), name="letter-create"),
     path("<uuid:letter_id>/update/", LetterUpdateApi.as_view(), name="letter-update"),
     path("<uuid:letter_id>/delete/", DeleteLetterApi.as_view(), name="letter-delete"),
+    path("<uuid:letter_id>/forward/", LetterForwardApi.as_view(), name="letter-forward"),
+    path("<uuid:letter_id>/approve/", LetterApproveApi.as_view(), name="letter-approve"),
 ]
