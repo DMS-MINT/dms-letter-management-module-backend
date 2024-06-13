9e8acd335ff28257f64535641c838282260230e9
Yab1 <yabilisanu@gmail.com>
2024-06-13 22:55:00 +0300

    refactor: Implemented display function for permissions in admin panel

--- core/participants/admin.py
+++ core/participants/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 
-from .models import Participant, Role
+from .models import Participant
 
 
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
-    list_display: list[str] = ["user", "role", "letter"]
+    list_display: list[str] = ["role_name", "user", "letter", "display_permissions"]
 
-
-@admin.register(Role)
-class RoleAdmin(admin.ModelAdmin):
-    list_display: list[str] = ["name", "permissions"]
+    def display_permissions(self, obj):
+        return ", ".join([permission.action for permission in obj.permissions.all()])
