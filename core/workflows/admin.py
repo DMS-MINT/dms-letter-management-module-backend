734958de3d0c86b9a970233cf6a6e336b9622e4e
Yab1 <yabilisanu@gmail.com>
2024-06-14 05:54:18 +0300

    chore: Registered Comment app to the api and the admin

--- core/workflows/admin.py
+++ core/workflows/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Comment
+from .models import WorkflowLog
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ["author", "content", "letter"]
+@admin.register(WorkflowLog)
+class WorkflowLogAdmin(admin.ModelAdmin):
+    list_display = ["user", "action", "resource", "initial_state", "final_state", "timestamp", "success"]
