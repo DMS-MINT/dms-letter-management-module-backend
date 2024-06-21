4e6707ca69bdc4b498e67ce926d5872524f19993
Yab1 <yabilisanu@gmail.com>
2024-06-21 14:00:58 +0300

    refactor: changed is_stuff to be false by default

--- core/users/models.py
+++ core/users/models.py
@@ -69,7 +69,7 @@ class Member(BaseUser, AbstractUser, PermissionsMixin):
     )
 
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
