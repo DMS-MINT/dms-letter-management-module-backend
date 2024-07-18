3c878f4942da6be1ca5e620d4242a967da4d2b41
Yab1 <yabilisanu@gmail.com>
2024-07-18 14:18:49 +0300

    feat(celery): configure Celery settings
    - set `CELERY_BROKER_URL` and `CELERY_RESULT_BACKEND` for Celery setup
    - configure Celery timezone, task time limits, and max retries
    - set Django settings module environment variable
    - initialize and configure Celery application with Django settings
    - enable automatic task discovery

---
+++ config/settings/celery.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+from config.env import env
+
+# https://docs.celeryproject.org/en/stable/userguide/configuration.html
+
+CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="amqp://guest:guest@localhost//")
+CELERY_RESULT_BACKEND = "django-db"
+
+CELERY_TIMEZONE = "UTC"
+
+CELERY_TASK_SOFT_TIME_LIMIT = 20  # seconds
+CELERY_TASK_TIME_LIMIT = 30  # seconds
+CELERY_TASK_MAX_RETRIES = 3
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.base")
+
+app = Celery("dms")
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks()
