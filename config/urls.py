541db625c1df418e627c3f05e40381e43ab8b8af
Yab1 <yabilisanu@gmail.com>
2024-08-02 10:05:40 +0300

    feat(urls): add PDF generation and letter display endpoints
    added endpoints for generating PDFs and displaying letters to the URL configuration.
    also included support for live reloading during development with django_browser_reload.

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from core.common.admin import dms_admin_site
+from core.puppeteer.views import display_letter, generate_pdf
 
 urlpatterns = [
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
@@ -12,6 +13,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(("core.api.urls", "api"), namespace="api")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("generate-pdf/", generate_pdf, name="generate_pdf"),
+    path("display-letter/", display_letter, name="display-letter"),
+    path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from config.settings.debug_toolbar.setup import DebugToolbarSetup  # noqa
