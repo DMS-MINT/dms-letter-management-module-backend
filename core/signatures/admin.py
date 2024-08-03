7156332fa19a4042ecec89b0d20ed50c2b3fd702
Yab1 <yabilisanu@gmail.com>
2024-08-03 22:55:34 +0300

    refactor(admin): update admin configuration for signature models
    replaced old SignatureAdmin with new configurations for LetterSignature and UserDefaultSignature. improved display, search, and filtering options, and added readonly fields handling.
    
    BREAKING CHANGE: updated admin configurations may require adjustments to existing admin interfaces.

--- core/signatures/admin.py
+++ core/signatures/admin.py
@@ -1,8 +1,59 @@
 from django.contrib import admin
 
-from .models import Signature
+from .models import LetterSignature, UserDefaultSignature
 
 
-@admin.register(Signature)
 class SignatureAdmin(admin.ModelAdmin):
-    list_display = ["id", "user"]
+    list_display = ("signer", "signature_method", "signature_image")
+    search_fields = ("signer__full_name",)
+    readonly_fields = ("signature_url",)
+    list_filter = ("signature_method",)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # If editing an existing object
+            return self.readonly_fields + ("signature_image",)
+        return self.readonly_fields
+
+    def signature_url(self, obj):
+        if obj.signature_image:
+            return obj.signature_url
+        return "No signature"
+
+    signature_url.short_description = "Signature URL"
+
+
+@admin.register(LetterSignature)
+class LetterSignatureAdmin(SignatureAdmin):
+    list_display = ("letter", "signer", "signature_method", "signature_image")
+    search_fields = ("letter__subject", "signer__full_name")
+    readonly_fields = SignatureAdmin.readonly_fields + ("letter",)
+
+
+@admin.register(UserDefaultSignature)
+class UserDefaultSignatureAdmin(admin.ModelAdmin):
+    list_display = ("user", "signature_image")
+    search_fields = ("user__full_name",)
+    readonly_fields = ("signature_url",)
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "user",
+                    "signature_image",
+                ),
+            },
+        ),
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # If editing an existing object
+            return self.readonly_fields + ("signature_image",)
+        return self.readonly_fields
+
+    def signature_url(self, obj):
+        if obj.signature_image:
+            return obj.signature_url
+        return "No signature"
+
+    signature_url.short_description = "Signature URL"
