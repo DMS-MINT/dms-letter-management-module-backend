580e26a509e34ca9a4cad59e14d9a921ead94b6c
Yab1 <yabilisanu@gmail.com>
2024-08-03 22:48:36 +0300

    feat(signatures): enhance signature models with dynamic upload paths and methods
    introduced dynamic upload paths for signature images based on the type of instance. added signature method choices and updated the Signature model to include method and URL properties.
    
    BREAKING CHANGE: the signature image upload path is now dynamic based on the instance type, requiring updates to existing data paths.

--- core/signatures/models.py
+++ core/signatures/models.py
@@ -1,26 +1,96 @@
+import os
+
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from core.comments.models import BaseModel
-from core.users.models import Member
+
+
+def signature_directory_path(instance, filename):
+    if hasattr(instance, "user"):
+        # For UserDefaultSignature
+        department = instance.user.department.name_en
+        user_id = instance.user.id
+        return f"users/{department}/{user_id}/default_signature.png"
+
+    if hasattr(instance, "letter"):
+        # For LetterSignature
+        department = instance.signer.department.name_en
+        letter_id = instance.letter.id
+        user_id = instance.signer.id
+        return f"letters/{department}/{letter_id}/signatures/{user_id}.png"
+
+    if hasattr(instance, "document"):
+        # For DocumentSignature
+        department = instance.signer.department.name_en
+        letter_id = instance.letter.id
+        user_id = instance.signer.id
+        return f"documents/{department}/{letter_id}/signatures/{user_id}.png"
+
+    # Default fallback path
+    return f"fallback/signatures/{filename}"
+
+
+def user_directory_path(instance, filename):
+    return f"signatures/{instance.user.department.name_en}/user_{instance.user.id}/signatures/default_signature.png"
 
 
 class Signature(BaseModel):
-    user = models.OneToOneField(
-        Member,
+    class Methods(models.IntegerChoices):
+        Default = 1, _("Default")
+        Canvas = 2, _("Canvas")
+
+    signer = models.ForeignKey(
+        "users.Member",
         on_delete=models.CASCADE,
-        related_name="e_signature",
-        help_text=_("Select the user associated with this signature."),
+        related_name="signed_%(class)s",
+        verbose_name=_("Signer"),
     )
-    e_signature = models.ImageField(
-        _("E Signature"),
-        unique=True,
-        upload_to="letters/signatures/",
-        help_text=_("Upload your signature image file."),
+    signature_image = models.ImageField(upload_to=signature_directory_path, verbose_name=_("Signature Image"))
+    signature_method = models.IntegerField(choices=Methods.choices, verbose_name=_("Signature Method"))
+
+    @property
+    def signature_url(self):
+        if self.signature_image:
+            return os.path.join(settings.MEDIA_URL, self.signature_image.name)
+        return ""
+
+    def __str__(self):
+        return f"{self.signer.full_name}'s signature"
+
+    class Meta:
+        abstract = True
+
+
+class LetterSignature(Signature):
+    letter = models.ForeignKey(
+        "letters.Letter",
+        on_delete=models.CASCADE,
+        related_name="e_signatures",
+        verbose_name=_("Letter"),
     )
+
+    class Meta(Signature.Meta):
+        db_table = "letter_signature"
+        verbose_name = _("Letter Signature")
+        verbose_name_plural = _("Letter Signatures")
+
+
+class UserDefaultSignature(BaseModel):
+    user = models.ForeignKey(
+        "users.Member",
+        on_delete=models.CASCADE,
+        related_name="default_signature",
+        verbose_name=_("User"),
+    )
+    signature_image = models.ImageField(upload_to=signature_directory_path, verbose_name=_("Signature Image"))
+
+    @property
+    def signature_url(self):
+        if self.signature_image:
+            return os.path.join(settings.MEDIA_URL, self.signature_image.name)
+        return ""
 
     def __str__(self):
         return f"{self.user.full_name}'s signature"
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
