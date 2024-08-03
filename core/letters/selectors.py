efa45b31415522e6d1a805924a67f3a3fe2008f6
Yab1 <yabilisanu@gmail.com>
2024-08-03 22:47:38 +0300

    feat(pdf): add PDF generation functionality for letters and reports
    added letter_pdf function to generate PDFs for different letter types and a generate_pdf function for general reports. these functions use templates to create PDF files based on letter or report content.
    
    BREAKING CHANGE: the PDF generation relies on specific HTML templates, which must be created or adjusted accordingly.

--- core/letters/selectors.py
+++ core/letters/selectors.py
@@ -1,7 +1,13 @@
+import io
+
+from django.template.loader import render_to_string
+from weasyprint import HTML
+
+from core.participants.models import Participant
 from core.users.models import Member
 
 from .filters import BaseLetterFilter
-from .models import Letter
+from .models import Incoming, Internal, Letter, Outgoing
 
 
 def letter_list(*, current_user=Member, filters=None):
@@ -10,3 +16,45 @@ def letter_list(*, current_user=Member, filters=None):
     qs = Letter.objects.filter(hidden=False)
 
     return BaseLetterFilter(filters, qs, current_user=current_user).qs
+
+
+def letter_pdf(*, letter_instance: Letter):
+    primary_recipients = letter_instance.participants.filter(role=Participant.Roles.PRIMARY_RECIPIENT)
+    cc_participants = letter_instance.participants.filter(role=Participant.Roles.CC)
+    bcc_participants = letter_instance.participants.filter(role=Participant.Roles.BCC)
+
+    context = {
+        "letter": letter_instance,
+        "primary_recipients": primary_recipients,
+        "cc_participants": cc_participants,
+        "bcc_participants": bcc_participants,
+    }
+
+    if isinstance(letter_instance, Internal):
+        template_name = "internal_letter_template.html"
+    elif isinstance(letter_instance, Outgoing):
+        template_name = "outgoing_letter_template.html"
+    elif isinstance(letter_instance, Incoming):
+        template_name = "incoming_letter_template.html"
+    else:
+        raise ValueError("Unknown letter type")
+
+    html_string = render_to_string(template_name, context)
+
+    return HTML(string=html_string).write_pdf()
+
+
+def generate_pdf():
+    template_name = "report.html"
+    context = {
+        "title": "My Report Title",
+        "body": "This is the body of the report.",
+    }
+
+    html_string = render_to_string(template_name=template_name, context=context)
+
+    pdf_io = io.BytesIO()
+    HTML(string=html_string).write_pdf(pdf_io)
+    pdf_io.seek(0)
+
+    return pdf_io
