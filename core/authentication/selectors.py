a67a2b43a2c899aa09c3b57813d50486de569c23
Yab1 <yabilisanu@gmail.com>
2024-08-04 19:41:10 +0300

    feat(user): expand department details in login data
    updated the `user_get_login_data` function to include detailed department information.
    this enhancement provides additional attributes like department names in different languages,
    abbreviations, description, contact phone, and contact email.

--- core/authentication/selectors.py
+++ core/authentication/selectors.py
@@ -2,7 +2,7 @@ from core.users.models import Member
 
 
 def user_get_login_data(*, current_user: Member):
-    department = current_user.department.name
+    department = current_user.department
 
     return {
         "id": getattr(current_user, "id", ""),
@@ -11,7 +11,15 @@ def user_get_login_data(*, current_user: Member):
         "last_name": getattr(current_user, "last_name", ""),
         "full_name": getattr(current_user, "full_name", ""),
         "job_title": getattr(current_user, "job_title", ""),
-        "department": department,
+        "department": {
+            "name_en": getattr(department, "name_en", ""),
+            "name_am": getattr(department, "name_am", ""),
+            "abbreviation_en": getattr(department, "abbreviation_en", ""),
+            "abbreviation_am": getattr(department, "abbreviation_am", ""),
+            "description": getattr(department, "description", ""),
+            "contact_phone": getattr(department, "contact_phone", None),
+            "contact_email": getattr(department, "contact_email", ""),
+        },
         "phone_number": getattr(current_user, "phone_number", ""),
         "is_staff": getattr(current_user, "is_staff", False),
         "is_2fa_enabled": getattr(current_user, "is_2fa_enabled", False),
