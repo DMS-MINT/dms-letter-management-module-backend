b1e7e75d74b3d5d91492ceabb4b02c26f1cbf9f7
Yab1 <yabilisanu@gmail.com>
2024-08-05 20:23:19 +0300

    feat(signature): enhance sign_letter with default signature and method
    updated the sign_letter function to use default user signature and allow specifying
    signature method. added utility to get signature method value from enum and created
    new LetterSignature instance with this information.

--- core/signatures/services.py
+++ core/signatures/services.py
@@ -1,27 +1,28 @@
 from django.db import transaction
-from rest_framework import status as http_status
 
-from core.api.exceptions import APIError
+from core.common.utils import get_object
 from core.letters.models import Letter
+from core.signatures.models import LetterSignature, Signature, UserDefaultSignature
 from core.users.models import Member
 
-from .models import Signature
+
+def get_enum_value(key: str) -> int:
+    for method in Signature.Methods:
+        if method.label.lower() == key.lower():
+            return method.value
+    raise ValueError(f"No matching signing method value for key: {key}")
 
 
 @transaction.atomic
-def sign_letter(*, letter_instance: Letter, current_user: Member):
-    try:
-        e_signature_instance = Signature.objects.get(user=current_user)
+def sign_letter(*, letter_instance: Letter, current_user: Member, signature_method: str):
+    user_default_signature = get_object(UserDefaultSignature, user=current_user)
+    signature_method_value = get_enum_value(signature_method)
 
-    except Signature.DoesNotExist:
-        raise APIError(
-            error_code="SIGNATURE_NOT_FOUND",
-            status_code=http_status.HTTP_404_NOT_FOUND,
-            message="Not Found Error",
-            extra={"e_signature": "The signature for the specified user does not exist."},
-        )
-
-    letter_instance.e_signature.add(e_signature_instance)
-    letter_instance.save()
+    LetterSignature.objects.create(
+        signer=current_user,
+        signature_method=signature_method_value,
+        signature_image=user_default_signature.signature_image,
+        letter=letter_instance,
+    )
 
     return letter_instance
