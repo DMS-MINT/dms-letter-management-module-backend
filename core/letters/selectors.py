2a82d7bfbd68438316c426b38cc0ab17bad84c20
Yab1 <yabilisanu@gmail.com>
2024-08-05 20:18:13 +0300

    feat(pdf): add e_signatures and base_url to PDF generation
    updated the letter_pdf function to include e_signatures and base_url in the PDF context. this allows for dynamic inclusion of electronic signatures and proper URL resolution in the generated PDFs.
    
    BREAKING CHANGE: the letter_pdf function now requires a letter_instance without type hints and additional context parameters.

--- core/letters/selectors.py
+++ core/letters/selectors.py
@@ -1,8 +1,9 @@
-import io
-
+from django.conf import settings
 from django.template.loader import render_to_string
+from django_weasyprint.utils import django_url_fetcher
 from weasyprint import HTML
 
+from config.env import BASE_URL
 from core.participants.models import Participant
 from core.users.models import Member
 
@@ -18,7 +19,7 @@ def letter_list(*, current_user=Member, filters=None):
     return BaseLetterFilter(filters, qs, current_user=current_user).qs
 
 
-def letter_pdf(*, letter_instance: Letter):
+def letter_pdf(letter_instance):
     primary_recipients = letter_instance.participants.filter(role=Participant.Roles.PRIMARY_RECIPIENT)
     cc_participants = letter_instance.participants.filter(role=Participant.Roles.CC)
     bcc_participants = letter_instance.participants.filter(role=Participant.Roles.BCC)
@@ -28,6 +29,8 @@ def letter_pdf(*, letter_instance: Letter):
         "primary_recipients": primary_recipients,
         "cc_participants": cc_participants,
         "bcc_participants": bcc_participants,
+        "e_signatures": letter_instance.e_signatures.all(),
+        "base_url": BASE_URL,
     }
 
     if isinstance(letter_instance, Internal):
@@ -39,6 +42,14 @@ def letter_pdf(*, letter_instance: Letter):
     else:
         raise ValueError("Unknown letter type")
 
-    html_string = render_to_string(template_name, context)
+    html_content = render_to_string(template_name=template_name, context=context)
 
-    return HTML(string=html_string).write_pdf()
+    weasy_html = HTML(
+        string=html_content,
+        url_fetcher=django_url_fetcher,
+        base_url=settings.STATIC_URL,
+    )
+
+    pdf_content = weasy_html.write_pdf()
+
+    return pdf_content
