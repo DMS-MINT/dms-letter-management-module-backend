0477c1dcfc237e96bd26a4d91eabe81e8330e4c5
Yab1 <yabilisanu@gmail.com>
2024-08-06 23:09:44 +0300

    feat(pdf): enhance PDF generation task with improved context and error handling
    - updated PDF generation to use specific templates based on letter type.
    - included participants' roles and e-signatures in the PDF context.
    - integrated django_url_fetcher for better URL handling in PDFs.
    - added logging for task start and completion, with improved error handling.
    - refined file path naming and ensured existing files are replaced.
    
    BREAKING CHANGE: the PDF generation process now requires updated templates and context.

--- core/letters/tasks.py
+++ core/letters/tasks.py
@@ -1,45 +1,86 @@
 import io
 
+from celery import shared_task
 from celery.utils.log import get_task_logger
+from django.conf import settings
 from django.core.files.storage import default_storage
 from django.template.loader import render_to_string
+from django_weasyprint.utils import django_url_fetcher
+from rest_framework.exceptions import ValidationError
 from weasyprint import HTML
 
-from config import shared_task
+from config.env import BASE_URL
 from core.common.utils import get_object
+from core.participants.models import Participant
+
+from .models import Incoming, Internal, Letter, Outgoing
 
 logger = get_task_logger(__name__)
 
 
-@shared_task(bind=True, max_retries=3)
-def letter_generate_pdf_task(self, letter_id: str):
-    from core.letters.models import Letter
-
+@shared_task(bind=True)
+def generate_pdf_task(self, letter_id: str) -> Letter:
     letter_instance = get_object(Letter, id=letter_id)
 
     try:
-        template_name = "report.html"
+        logger.info(f"Starting PDF generation for reference number: {letter_instance.reference_number}")
+
+        authors = letter_instance.participants.filter(role=Participant.Roles.AUTHOR)
+        primary_recipients = letter_instance.participants.filter(role=Participant.Roles.PRIMARY_RECIPIENT)
+        cc_participants = letter_instance.participants.filter(role=Participant.Roles.CC)
+        bcc_participants = letter_instance.participants.filter(role=Participant.Roles.BCC)
+
         context = {
-            "title": "My Report Title",
-            "body": "This is the body of the report.",
+            "letter": letter_instance,
+            "authors": authors,
+            "primary_recipients": primary_recipients,
+            "cc_participants": cc_participants,
+            "bcc_participants": bcc_participants,
+            "e_signatures": letter_instance.e_signatures.all(),
+            "base_url": BASE_URL,
         }
 
-        html_string = render_to_string(template_name=template_name, context=context)
+        if isinstance(letter_instance, Internal):
+            template_name = "internal_letter_template.html"
+        elif isinstance(letter_instance, Outgoing):
+            template_name = "outgoing_letter_template.html"
+        elif isinstance(letter_instance, Incoming):
+            template_name = "incoming_letter_template.html"
+        else:
+            raise ValueError("Unknown letter type")
+
+        html_content = render_to_string(template_name=template_name, context=context)
 
         pdf_io = io.BytesIO()
-        HTML(string=html_string).write_pdf(pdf_io)
+        weasy_html = HTML(
+            string=html_content,
+            url_fetcher=django_url_fetcher,
+            base_url=settings.STATIC_URL,
+        )
+
+        weasy_html.write_pdf(pdf_io)
         pdf_io.seek(0)
 
         department = letter_instance.owner.department.name_en
         letter_ref_no = letter_instance.reference_number
 
-        letter_pdf_path = f"letters/{department}/letter-{letter_ref_no}/letter-{letter_ref_no}.pdf"
+        letter_pdf_path = f"letters/{department}/letter_{letter_ref_no}/letter_{letter_ref_no}.pdf"
+
+        if default_storage.exists(letter_pdf_path):
+            default_storage.delete(letter_pdf_path)
+
         default_storage.save(letter_pdf_path, pdf_io)
 
         letter_instance.pdf_version = default_storage.url(letter_pdf_path)
         letter_instance.save(update_fields=["pdf_version"])
 
+        logger.info(f"PDF generation completed successfully for reference number: {letter_instance.reference_number}")
+        return None
+
+    except ValueError as e:
+        logger.error(f"Error generating PDF for reference number: {letter_id}, error: {e}")
+        raise ValidationError(e)
+
     except Exception as e:
-        # https://docs.celeryq.dev/en/stable/userguide/tasks.html#retrying
-        logger.error(f"An error occurred while generating the PDF for letter id {letter_id}: {e}")
-        self.retry(exc=e, countdown=5)
+        logger.error(f"Error generating PDF for reference number: {letter_id}, error: {e}")
+        raise e
