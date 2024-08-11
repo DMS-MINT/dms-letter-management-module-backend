10d94950648dd706521ec35f75193a1194e19bdf
Yab1 <yabilisanu@gmail.com>
2024-08-11 19:43:07 +0300

    refactor(admin): enhance participant admin with polymorphic models
    - refactored participant admin to use polymorphic models for better organization.
    - created separate admin classes for different participant types.
    - used `PolymorphicParentModelAdmin` and `PolymorphicChildModelAdmin` for handling inheritance.

--- core/participants/admin.py
+++ core/participants/admin.py
@@ -1,11 +1,39 @@
 from django.contrib import admin
+from polymorphic.admin import (
+    PolymorphicChildModelAdmin,
+    PolymorphicChildModelFilter,
+    PolymorphicParentModelAdmin,
+)
 
-from .models import Participant
+from .models import BaseParticipant, ExternalUserParticipant, InternalUserParticipant, PublicEnterpriseParticipant
 
 
-@admin.register(Participant)
-class ParticipantAdmin(admin.ModelAdmin):
-    list_display: list[str] = ["role", "user", "letter"]
+class ParticipantChildAdmin(PolymorphicChildModelAdmin):
+    base_model = BaseParticipant
+    list_display: list[str] = ("role", "letter")
 
-    def display_permissions(self, obj):
-        return ", ".join([permission.name for permission in obj.permissions.all()])
+
+@admin.register(InternalUserParticipant)
+class InternalParticipantAdmin(ParticipantChildAdmin):
+    base_model = InternalUserParticipant
+    show_in_index = True
+
+
+@admin.register(PublicEnterpriseParticipant)
+class PublicEnterpriseParticipantAdmin(ParticipantChildAdmin):
+    base_model = PublicEnterpriseParticipant
+    show_in_index = True
+
+
+@admin.register(ExternalUserParticipant)
+class ExternalUserParticipantAdmin(ParticipantChildAdmin):
+    base_model = ExternalUserParticipant
+    show_in_index = True
+
+
+@admin.register(BaseParticipant)
+class BaseParticipantAdmin(PolymorphicParentModelAdmin):
+    base_model: BaseParticipant
+    list_display: list[str] = ("role", "letter")
+    child_models = (InternalUserParticipant, PublicEnterpriseParticipant, ExternalUserParticipant)
+    list_filter = (PolymorphicChildModelFilter,)
