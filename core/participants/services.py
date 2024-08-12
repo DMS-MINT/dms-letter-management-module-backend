c9a70f49377f2a1d87c0d5cc3c76b0f4c763bf2c
Yab1 <yabilisanu@gmail.com>
2024-08-12 21:17:33 +0300

    refactor(participants-services): enhance participant creation with support for multiple types
    - updated participant_instance_create to handle users, enterprises, and contacts.
    - introduced new participant models: EnterpriseParticipant and ExternalUserParticipant.
    - removed deprecated handling for guest users and simplified participant creation logic.
    - adjusted participants_create function to accommodate new participant types.

--- core/participants/services.py
+++ core/participants/services.py
@@ -6,47 +6,71 @@ from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import PermissionDenied
 
 from core.comments.services import comment_create
+from core.contacts.services import contact_create
+from core.enterprises.models import Enterprise
 from core.letters.models import Letter
 from core.users.models import User
 
-from .models import BaseParticipant
-from .utils import get_enum_value, verify_owners_role
+from .models import BaseParticipant, EnterpriseParticipant, ExternalUserParticipant, InternalUserParticipant
+from .utils import get_enum_value
 
 type LetterParticipant = dict[str, Union[str, int, dict[str, str], list[str]]]
 
 
 @transaction.atomic
 def participant_instance_create(
-    current_user: User,
-    target_user_id: str,
-    user_type: str,
-    letter_instance: Letter,
     role: int,
+    participant,
+    current_user: User,
+    letter_instance: Letter,
     permissions: list[str] = None,
 ):
-    # user_instance_classes = {"User": User, "guest": Guest}.get(user_type)
-    user_instance_classes = {"User": User}.get(user_type)
+    participant_type = participant.pop("participant_type")
 
-    if user_instance_classes == User:
-        user = user_instance_classes.objects.get(pk=target_user_id)
+    if participant_type == "user":
+        user = User.objects.get(pk=participant["user_id"])
 
         if role == BaseParticipant.Roles.ADMINISTRATOR and not user.is_staff:
             raise PermissionDenied("Cannot assign administrator privileges to a non-staff user.")
 
-    # elif user_instance_classes == Guest:
-    #     user, _ = user_instance_classes.objects.get_or_create(name=target_user_id)
+        participant_instance = InternalUserParticipant.objects.create(
+            user=user,
+            role=role,
+            letter=letter_instance,
+            added_by=current_user,
+        )
+
+        participant_instance.save(permissions=permissions)
+
+    elif participant_type == "enterprise":
+        enterprise = Enterprise.objects.get(pk=participant["enterprise_id"])
+
+        participant_instance = EnterpriseParticipant.objects.create(
+            enterprise=enterprise,
+            role=role,
+            letter=letter_instance,
+            added_by=current_user,
+        )
+
+    elif participant_type == "contact":
+        contact = contact_create(
+            current_user=current_user,
+            full_name_en=participant["contact"]["full_name_en"],
+            full_name_am=participant["contact"]["full_name_am"],
+            email=participant["contact"]["email"],
+            phone_number=participant["contact"]["phone_number"],
+            address=participant["contact"]["address"],
+        )
+
+        participant_instance = ExternalUserParticipant.objects.create(
+            contact=contact,
+            role=role,
+            letter=letter_instance,
+            added_by=current_user,
+        )
 
     else:
-        raise ValueError("Invalid user type")
-
-    participant_instance = BaseParticipant.objects.create(
-        user=user,
-        role=role,
-        letter=letter_instance,
-        added_by=current_user,
-    )
-
-    participant_instance.save(permissions=permissions)
+        raise ValueError("Invalid participant type. The allowed types are user, enterprise, or contact.")
 
 
 # This function create participants for a given letter.
@@ -57,15 +81,12 @@ def participants_create(
     letter_instance: Letter,
     participants,
 ):
-    participants = verify_owners_role(letter_instance=letter_instance, participants=participants)
+    # participants = verify_owners_role(letter_instance=letter_instance, participants=participants)
 
     for participant in participants:
-        role_value = get_enum_value(participant["role"])
-
         participant_instance_create(
-            target_user_id=participant["user"]["id"],
-            user_type=participant["user"]["user_type"],
-            role=role_value,
+            role=participant.pop("role"),
+            participant=participant,
             letter_instance=letter_instance,
             current_user=current_user,
         )
