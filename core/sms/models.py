91029e3779cfbc0e8c04853662efc651b71d9de9
Yab1 <yabilisanu@gmail.com>
2024-08-15 00:04:10 +0300

    feat(sms): add SMS model inheriting from Notification
    - introduced the `SMS` model extending the `Notification` class for consistency
    - added a `message` field to store the SMS content
    - implemented `__str__` method to return the SMS message for better representation

--- core/sms/models.py
+++ core/sms/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from core.notifications.models import Notification
+
+
+class SMS(Notification):
+    message = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.message
