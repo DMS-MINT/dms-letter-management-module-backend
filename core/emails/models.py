a158837f45e3a006e18dcc0f57643548107f2250
Yab1 <yabilisanu@gmail.com>
2024-08-15 00:03:15 +0300

    refactor(email): simplify email model by extending Notification and removing status
    - refactored the `Email` model to inherit from `Notification` for consistency
    - removed the `status` field and associated choices as they are no longer needed
    - added a `__str__` method to return the email subject for better representation

--- core/emails/models.py
+++ core/emails/models.py
@@ -1,22 +1,13 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
-from core.common.models import BaseModel
+from core.notifications.models import Notification
 
 
-class Email(BaseModel):
-    class Status(models.TextChoices):
-        READY = "READY", _("Ready")
-        SENDING = "SENDING", _("Sending")
-        SENT = "SENT", _("Sent")
-        FAILED = "FAILED", _("Failed")
-
-    status = models.CharField(max_length=255, db_index=True, choices=Status.choices, default=Status.READY)
-
-    to = models.OneToOneField("users.User", on_delete=models.CASCADE, related_name="email_received")
+class Email(Notification):
     subject = models.CharField(max_length=255)
 
     html = models.TextField()
     plain_text = models.TextField()
 
-    sent_at = models.DateTimeField(blank=True, null=True)
+    def __str__(self):
+        return self.subject
