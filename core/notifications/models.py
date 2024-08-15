5eb9e2378f57ef85288b14b4f09c06f0d4c35c22
Yab1 <yabilisanu@gmail.com>
2024-08-15 10:02:41 +0300

    refactor(notification): update tag field and add action field to notification model
    - changed tag field from IntegerField to ManyToManyField with Tag model.
    - updated status field from IntegerField to CharField for improved readability.
    - added action field as a JSONField with a default empty dictionary to store action details.
    
    BREAKING CHANGE: existing migrations or data models may need adjustment due to schema changes.

--- core/notifications/models.py
+++ core/notifications/models.py
@@ -4,40 +4,40 @@ from django.utils.translation import gettext_lazy as _
 from core.common.models import BaseModel
 
 
+class Tag(BaseModel):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Notification(BaseModel):
-    class Status(models.IntegerChoices):
-        READY = 1, _("Ready")
-        SENDING = 2, _("Sending")
-        SENT = 3, _("Sent")
-        FAILED = 4, _("Failed")
+    class Status(models.TextChoices):
+        READY = "ready", _("Ready")
+        SENDING = "sending", _("Sending")
+        SENT = "sent", _("Sent")
+        FAILED = "failed", _("Failed")
 
-    class Tags(models.IntegerChoices):
-        MENTION = 1, _("Mention")
-        INBOX = 2, _("Inbox")
-        WORKFLOW = 3, _("Workflow")
-        PING = 4, _("Ping")
-        REMINDER = 5, _("Reminder")
-        COMMENT = 6, _("Comment")
-
-    status = models.IntegerField(db_index=True, choices=Status.choices, default=Status.READY)
+    status = models.CharField(db_index=True, max_length=50, choices=Status.choices, default=Status.READY)
 
     to = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="%(class)s_received")
     sender = models.ForeignKey(
         "users.User",
         on_delete=models.SET_NULL,
+        related_name="%(class)s_sent",
         null=True,
         blank=True,
-        related_name="%(class)s_sent",
     )
 
     sent_at = models.DateTimeField(blank=True, null=True)
-    tag = models.IntegerField(choices=Tags.choices, default=Status.READY)
+    tag = models.ManyToManyField(Tag, related_name="%(class)s_tagged_notifications", blank=True)
+    action = models.JSONField(default=dict)
 
     class Meta:
         abstract = True
 
 
-class InAppNotification(Notification):
+class InApp(Notification):
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
     is_notified = models.BooleanField(default=False)
