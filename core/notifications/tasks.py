809d4e9c2d2d9ac34f1fa94e0ccc3c9b7e271930
Yab1 <yabilisanu@gmail.com>
2024-08-16 17:31:56 +0300

    feat(tasks): add Celery task for sending notifications
    - implemented `send_notifications_task` to handle notification sending asynchronously.
    - updated notification status to `SENDING` and recorded the sent timestamp.
    - added logging for notification existence and exception handling.

--- core/notifications/tasks.py
+++ core/notifications/tasks.py
@@ -1,22 +1,32 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
+from django.utils import timezone
 
 from core.common.utils import get_object
 
-from .models import InAppNotification
+from .models import Notification
 from .services import notification_send
 
 logger = get_task_logger(__name__)
 
 
 @shared_task(bind=True)
-def notification_send_task(self, notification_id: str):
-    notification_instance = get_object(InAppNotification, pk=notification_id)
-
+def send_notifications_task(
+    self,
+    notification_id: str,
+    email_id: str = None,
+    sms_id: str = None,
+):
     try:
-        notification_send(notification_instance)
+        notification_instance = get_object(Notification, pk=notification_id)
 
-    except InAppNotification.DoesNotExist:
+        if notification_instance.status == Notification.Status.READY:
+            notification_instance.status = Notification.Status.SENDING
+            notification_instance.sent_at = timezone.now()
+            notification_instance.save()
+            notification_send(notification_instance=notification_instance)
+
+    except Notification.DoesNotExist:
         logger.warning(f"Notification with id {notification_id} does not exist.")
 
     except Exception as e:
