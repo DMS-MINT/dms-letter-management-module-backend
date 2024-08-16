c7a5f63ac3dc379d7732004e7cb3621390400f42
Yab1 <yabilisanu@gmail.com>
2024-08-16 17:30:02 +0300

    refactor(notifications): update notification handling and creation logic
    - refactored notification_send to handle sending notifications in the correct state.
    - added notification_create to streamline notification creation with channels and recipients.
    - introduced notification_recipient_create to efficiently bulk create notification recipients.
    - updated notification_send to use channels and async methods for sending notifications.
    
    BREAKING CHANGE: notification handling logic has been refactored, which may require updates to integration points.

--- core/notifications/services.py
+++ core/notifications/services.py
@@ -1,15 +1,83 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.db import transaction
 from django.utils import timezone
 
 from core.api.exceptions import ApplicationError
+from core.notifications.serializers import NotificationSerializer
 from core.users.models import User
 
-from .models import InAppNotification, Notification
+from .models import ChannelType, Notification, NotificationRecipient, Tag
+
+
+@transaction.atomic()
+def notification_create(
+    *,
+    subject: str,
+    tags: list[Tag],
+    channels: list[str],
+    to: list[str],
+    details: dict,
+    message: str,
+):
+    channels = ChannelType.objects.filter(channel_type__in=channels)
+    notification_instance = Notification.objects.create(
+        status=Notification.Status.READY,
+        details=details,
+        subject=subject,
+        message=message,
+    )
+
+    notification_instance.tags.set(tags)
+    notification_instance.channels.set(channels)
+
+    notification_recipient_create(notification_instance=notification_instance, to=to)
+
+    return notification_instance
+
+
+@transaction.atomic()
+def notification_recipient_create(*, notification_instance: Notification, to: list[str]):
+    users = User.objects.filter(id__in=to)
+
+    NotificationRecipient.objects.bulk_create(
+        NotificationRecipient(
+            notification=notification_instance,
+            user=user,
+        )
+        for user in users
+    )
 
 
 @transaction.atomic
-def notification_send(notification_instance: InAppNotification):
-    if notification_instance.status != Notification.Status.SENDING:
-        raise ApplicationError(f"Cannot send non-ready notifications. Current status is {notification_instance.status}")
+def notification_send(*, notification_instance: Notification):
+    if notification_instance.status != Notification.Status.READY:
+        raise ApplicationError(
+            f"Cannot send non-ready notifications. Current status is {notification_instance.status}",
+        )
 
-    # CALL THE CHANNEL FROM HERE
+    notification_instance.status = Notification.Status.SENDING
+    notification_instance.sent_at = timezone.now()
+    notification_instance.save()
+
+    recipients = notification_instance.notification_recipients.all()
+
+    for recipient in recipients:
+        output_serializer = NotificationSerializer(
+            notification_instance,
+            context={"user": recipient.user},
+        )
+
+        group_name = f"user_{recipient.user.id}"
+        channel_layers = get_channel_layer()
+        async_to_sync(channel_layers.group_send)(
+            group_name,
+            {
+                "type": "send_notification",
+                "message": output_serializer.data,
+            },
+        )
+
+    notification_instance.status = Notification.Status.SENT
+
+    return notification_instance
