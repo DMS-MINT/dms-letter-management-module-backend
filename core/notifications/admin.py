603c773c8ea1d894ca880f834fb9ad6bc8c5045f
Yab1 <yabilisanu@gmail.com>
2024-08-16 17:28:19 +0300

    feat(notifications): extend notification system with new models
    - added ChannelType model to define notification channels.
    - updated Notification model to include channels, message, subject, and HTML content.
    - introduced NotificationRecipient model to track recipients and read status.
    - modified existing relationships and field names to support new features.
    
    BREAKING CHANGE: the notification system schema has been updated, affecting existing data and relationships.

--- core/notifications/admin.py
+++ core/notifications/admin.py
@@ -1,12 +1,68 @@
 from django.contrib import admin
 
-from .models import Tag
+from .models import ChannelType, Notification, NotificationRecipient, Tag
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ("name",)
-    ordering = ("name",)
     search_fields = ("name",)
-    list_filter = ("name",)
-    list_display_links = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(ChannelType)
+class ChannelTypeAdmin(admin.ModelAdmin):
+    list_display = ("channel_type",)
+    search_fields = ("channel_type",)
+    ordering = ("channel_type",)
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "subject",
+        "status",
+        "sent_at",
+    )
+    search_fields = (
+        "subject",
+        "message",
+    )
+    list_filter = (
+        "status",
+        "channels",
+    )
+    ordering = ("-sent_at",)
+    filter_horizontal = (
+        "channels",
+        "tags",
+    )
+
+    readonly_fields = (
+        "status",
+        "sent_at",
+    )
+
+
+@admin.register(NotificationRecipient)
+class NotificationRecipientAdmin(admin.ModelAdmin):
+    list_display = (
+        "notification",
+        "user",
+        "has_read",
+        "has_notified",
+    )
+    search_fields = (
+        "user__username",
+        "notification__subject",
+    )
+    list_filter = (
+        "has_read",
+        "has_notified",
+    )
+    ordering = ("-notification__sent_at",)
+
+    readonly_fields = (
+        "notification",
+        "user",
+    )
--- core/notifications/models.py
+++ core/notifications/models.py
@@ -11,7 +11,19 @@ class Tag(BaseModel):
         return self.name
 
 
-class BaseNotification(BaseModel):
+class ChannelType(BaseModel):
+    class Channels(models.TextChoices):
+        IN_APP = "in_app", _("In-App")
+        EMAIL = "email", _("Email")
+        SMS = "sms", _("SMS")
+
+    channel_type = models.CharField(max_length=10, choices=Channels.choices, unique=True)
+
+    def __str__(self):
+        return self.get_channel_type_display()
+
+
+class Notification(BaseModel):
     class Status(models.TextChoices):
         READY = "ready", _("Ready")
         SENDING = "sending", _("Sending")
@@ -19,19 +31,29 @@ class BaseNotification(BaseModel):
         FAILED = "failed", _("Failed")
 
     status = models.CharField(db_index=True, max_length=50, choices=Status.choices, default=Status.READY)
+    channels = models.ManyToManyField(ChannelType, related_name="typed_notifications")
 
-    to = models.ManyToManyField("users.User", related_name="%(class)s_received")
-    sender = models.ForeignKey(
-        "users.User",
-        on_delete=models.SET_NULL,
-        related_name="%(class)s_sent",
-        null=True,
-        blank=True,
-    )
+    message = models.CharField(max_length=255, blank=True, null=True)
+    subject = models.CharField(max_length=255, blank=True, null=True)
+    html_content = models.TextField(blank=True, null=True)
+
+    tags = models.ManyToManyField("Tag", related_name="tagged_notifications", blank=True)
+    details = models.JSONField(default=dict)
 
     sent_at = models.DateTimeField(blank=True, null=True)
-    tag = models.ManyToManyField(Tag, related_name="%(class)s_tagged_notifications", blank=True)
-    action = models.JSONField(default=dict)
+
+    def __str__(self):
+        return self.subject
+
+
+class NotificationRecipient(BaseModel):
+    notification = models.ForeignKey(Notification, on_delete=models.CASCADE, related_name="notification_recipients")
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="notifications_received")
+    has_read = models.BooleanField(default=False)
+    has_notified = models.BooleanField(default=False)
 
     class Meta:
-        abstract = True
+        unique_together = ("notification", "user")
+
+    def __str__(self):
+        return f"{self.user.full_name} notifications"
