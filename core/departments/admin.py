c681b4bee8768a0afb331d3b065962bec95e89bc
Yab1 <yabilisanu@gmail.com>
2024-08-16 20:18:25 +0300

    refactor(models): update Department model fields and add JobTitle model
    - renamed fields in `Department` model to use more descriptive names and added verbose names.
    - updated `Department` model meta options for better readability and consistency.
    - added `JobTitle` model with English and Amharic titles.
    - set verbose names for fields in the new `JobTitle` model.

--- core/departments/admin.py
+++ core/departments/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from .models import Department
+from .models import Department, JobTitle
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ("name_en", "name_am", "created_at", "updated_at")
-    ordering = ["name_en"]
+    list_display = (
+        "department_name_en",
+        "department_name_am",
+        "abbreviation_en",
+        "abbreviation_am",
+        "created_at",
+        "updated_at",
+    )
+    ordering = ["department_name_en"]
+
+
+@admin.register(JobTitle)
+class JobTitleAdmin(admin.ModelAdmin):
+    list_display = ("title_en", "title_am", "created_at", "updated_at")
+    ordering = ["title_en"]
