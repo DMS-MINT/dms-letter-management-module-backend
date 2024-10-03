f8d35c83af70d1d4c42abef12e92671ca53feb3e
KaleabTm <kaltamiru8@gmail.com>
2024-10-03 11:58:15 +0300

    feat(signals): updated the email templates and updated the email sending upon user creation with signals

--- core/users/services.py
+++ core/users/services.py
@@ -26,30 +26,7 @@ def user_create(
 ) -> User:
     department_instance = Department.objects.get(department_name_en=department)
     job_title_instance = JobTitle.objects.get(title_en=job_title)
-
-    if password is None:
-        password = get_random_string(length=8)
-
-        email_send_type(
-            email, 
-            "Welcome to Our Service", 
-            "registration", 
-            context={
-                'username': email,
-                'default_password': password,
-                'first_name': first_name_en,
-                }
-            )
         
-    email_send_type(
-        email, 
-        "Welcome to Our Service", 
-        "welcome", 
-        context={
-            'first_name': first_name_en,
-            }
-        )
-    
     return (
         User.objects.create_user(
             first_name_en=first_name_en,
---
+++ core/users/signals.py
@@ -0,0 +1,40 @@
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+
+from core.emails.services import email_send_type 
+from .models import User
+from django.utils.crypto import get_random_string
+
+
+@receiver(pre_save,sender=User)
+def pre_save_handler(sender,instance,*arg,**kwarg):
+    if not instance.password:
+        instance.password = get_random_string(length=8)
+
+
+@receiver(post_save,sender=User)
+def post_save_handler(sender,instance,created,*args,**kwargs):
+    if created:
+        if not instance.password:
+            instance.password = get_random_string(length=8)
+            email_send_type(
+                instance.email, 
+                "Welcome to Our Service", 
+                "registration", 
+                context={
+                'username': instance.email,
+                'default_password': instance.password,
+                'first_name': instance.first_name_en,
+                }
+            )
+        else:
+            email_send_type(
+                instance.email, 
+                "Welcome to Our Service", 
+                "welcome", 
+                context={
+                'first_name': instance.first_name_en,
+                }
+            )
+        
+    
