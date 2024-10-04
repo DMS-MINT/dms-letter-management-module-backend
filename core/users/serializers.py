5ff995b340139a6de56a69b4b88c15d0d11d9ae7
yab1 <yabilisanu@gmail.com>
2024-10-04 10:55:50 +0300

    refactor(serializers): update user serializers to use inline serializers for nested fields
    - replaced inline_serializer with dedicated serializers for job titles and departments.
    - updated fields to use user_profile attributes for consistency.
    - changed phone_number to a CharField in CurrentUserSerializer for better validation.

--- core/users/serializers.py
+++ core/users/serializers.py
@@ -1,72 +1,41 @@
 from rest_framework import serializers
 
-from core.common.utils import inline_serializer
+from core.departments.serializers import DepartmentSerializer, JobTitleSerializer
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    email = serializers.EmailField()
 
 
 class UserListSerializer(serializers.Serializer):
     id = serializers.UUIDField()
-    full_name_en = serializers.CharField()
-    full_name_am = serializers.CharField()
-    job_title = inline_serializer(
-        fields={
-            "title_en": serializers.CharField(),
-            "title_am": serializers.CharField(),
-        },
-    )
+    full_name_en = serializers.CharField(source="user_profile.full_name_en")
+    full_name_am = serializers.CharField(source="user_profile.full_name_am")
+    job_title = JobTitleSerializer(source="user_profile.job_title")
 
 
 class UserDetailSerializer(serializers.Serializer):
     id = serializers.UUIDField()
-    full_name_en = serializers.CharField()
-    full_name_am = serializers.CharField()
-    job_title = inline_serializer(
-        fields={
-            "id": serializers.UUIDField(),
-            "title_en": serializers.CharField(),
-            "title_am": serializers.CharField(),
-        },
-    )
-    department = inline_serializer(
-        fields={
-            "id": serializers.UUIDField(),
-            "department_name_en": serializers.CharField(),
-            "department_name_am": serializers.CharField(),
-        },
-    )
+    full_name_en = serializers.CharField(source="user_profile.full_name_en")
+    full_name_am = serializers.CharField(source="user_profile.full_name_am")
+    job_title = JobTitleSerializer(source="user_profile.job_title")
+    department = DepartmentSerializer(source="user_profile.department")
     phone_number = serializers.IntegerField()
     email = serializers.EmailField()
 
 
 class CurrentUserSerializer(serializers.Serializer):
     id = serializers.UUIDField()
-    first_name_en = serializers.CharField()
-    middle_name_en = serializers.CharField()
-    last_name_en = serializers.CharField()
-    first_name_am = serializers.CharField()
-    middle_name_am = serializers.CharField()
-    last_name_am = serializers.CharField()
-    full_name_en = serializers.CharField()
-    full_name_am = serializers.CharField()
-    job_title = inline_serializer(
-        fields={
-            "id": serializers.UUIDField(),
-            "title_en": serializers.CharField(),
-            "title_am": serializers.CharField(),
-        },
-    )
-    department = inline_serializer(
-        fields={
-            "id": serializers.UUIDField(),
-            "department_name_en": serializers.CharField(),
-            "department_name_am": serializers.CharField(),
-            "abbreviation_en": serializers.CharField(),
-            "abbreviation_am": serializers.CharField(),
-            "description": serializers.CharField(),
-            "contact_phone": serializers.IntegerField(),
-            "contact_email": serializers.EmailField(),
-        },
-    )
-    phone_number = serializers.IntegerField()
     email = serializers.EmailField()
-    is_2fa_enabled = serializers.BooleanField()
+    first_name_en = serializers.CharField(source="user_profile.first_name_en")
+    middle_name_en = serializers.CharField(source="user_profile.middle_name_en")
+    last_name_en = serializers.CharField(source="user_profile.last_name_en")
+    first_name_am = serializers.CharField(source="user_profile.first_name_am")
+    middle_name_am = serializers.CharField(source="user_profile.middle_name_am")
+    last_name_am = serializers.CharField(source="user_profile.last_name_am")
+    job_title = JobTitleSerializer(source="user_profile.job_title")
+    department = DepartmentSerializer(source="user_profile.department")
+    phone_number = serializers.CharField(source="user_profile.phone_number")
     is_staff = serializers.BooleanField()
+    is_2fa_enabled = serializers.BooleanField(source="user_settings.is_2fa_enabled")
