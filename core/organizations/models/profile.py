7331954ff4485d50a2e99154723ed0d6ebd2d880
yab1 <yabilisanu@gmail.com>
2024-10-04 10:53:39 +0300

    feat(organization): add organization profile model with extended details
    - created the OrganizationProfile model to store additional information.
    - included fields for description, contact details, and address.
    - added a method to provide a string representation of the organization profile.
    - set up related names for foreign key relationships for better query readability.

---
+++ core/organizations/models/profile.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from tenant_users.tenants.models import TenantBase
+
+from core.common.models import BaseModel
+
+
+def organization_directory_path(instance, filename):
+    return f"organizations/{instance.organization_name_en}/logo.png"
+
+
+class OrganizationProfile(TenantBase, BaseModel):
+    organization = models.ForeignKey(
+        "organizations.Organization",
+        on_delete=models.CASCADE,
+        related_name="organization_profile",
+    )
+    description = models.TextField(blank=True, null=True, verbose_name=_("Description"))
+    contact_phone = models.PositiveBigIntegerField(blank=True, null=True, verbose_name=_("Contact Phone"))
+    contact_email = models.EmailField(blank=True, null=True, verbose_name=_("Contact Email"))
+    address = models.ForeignKey(
+        "common.Address",
+        on_delete=models.CASCADE,
+        related_name="organization_address",
+        verbose_name=_("Address"),
+    )
+    postal_code = models.PositiveIntegerField(blank=True, null=True, verbose_name=_("Postal Code"))
+    logo = models.ImageField(upload_to=organization_directory_path, blank=True, null=True, verbose_name="Logo")
+
+    def __str__(self):
+        return f"{self.name_en}"
+
+    class Meta:
+        verbose_name = _("Organization Profile")
+        verbose_name_plural = _("Organization Profiles")
