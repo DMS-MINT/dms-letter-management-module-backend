1a92665e95f0b2c97cc523381f69eb1648b7eb59
yab1 <yabilisanu@gmail.com>
2024-10-04 10:47:18 +0300

    feat(user): add pre_save signal to generate OTP secret for new users
    - created a pre_save signal for the user model to generate and set a random OTP secret.
    - the signal ensures that the OTP secret is assigned using pyotp's random_base32 method when creating users.

---
+++ core/users/signals.py
@@ -0,0 +1,11 @@
+import pyotp
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from .models.user import User
+
+
+@receiver(pre_save, sender=User)
+def set_otp_secret(instance, created, **kwargs):
+    if instance.otp_secret:
+        instance.otp_secret = pyotp.random_base32()
