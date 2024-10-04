4b87b24e7619bc3ce422637a68cc0899aa772c03
yab1 <yabilisanu@gmail.com>
2024-10-04 10:59:12 +0300

    refactor(models): update references due to user model restructuring
    - moved user model into separate files, creating a new directory structure for user-related models.
    - updated all references to the user model across the codebase to reflect the new import paths.
    - ensured all user-related functionality remains intact after the directory and model changes.

--- core/comments/services.py
+++ core/comments/services.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 
 from core.letters.models import Letter
-from core.users.models import User
+from core.users.models.user import User
 
 from .models import Comment
 
