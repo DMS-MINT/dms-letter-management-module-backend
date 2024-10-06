16fda9d17955b8b726cf239a29a9614087d6be0b
yab1 <yabilisanu@gmail.com>
2024-10-06 10:32:06 +0300

    feat(serializers): update user serializers with default values and additional fields
    - added default values to fields in UserListSerializer and UserDetailSerializer.
    - included is_staff and is_superuser fields in UserListSerializer.
    - set default values for various fields in CurrentUserSerializer for consistency.
    - updated phone_number field in UserDetailSerializer to allow None as a default.

--- core/users/serializers.py
+++ core/users/serializers.py
@@ -10,32 +10,40 @@ class UserSerializer(serializers.Serializer):
 
 class UserListSerializer(serializers.Serializer):
     id = serializers.UUIDField()
-    full_name_en = serializers.CharField(source="user_profile.full_name_en")
-    full_name_am = serializers.CharField(source="user_profile.full_name_am")
-    job_title = JobTitleSerializer(source="user_profile.job_title")
+    full_name_en = serializers.CharField(source="user_profile.full_name_en", default=None)
+    full_name_am = serializers.CharField(source="user_profile.full_name_am", default=None)
+    job_title = JobTitleSerializer(source="user_profile.job_title", default=None)
+    department = DepartmentSerializer(source="user_profile.department", default=None)
+    is_staff = serializers.BooleanField()
+    is_superuser = serializers.BooleanField()
 
 
 class UserDetailSerializer(serializers.Serializer):
     id = serializers.UUIDField()
-    full_name_en = serializers.CharField(source="user_profile.full_name_en")
-    full_name_am = serializers.CharField(source="user_profile.full_name_am")
-    job_title = JobTitleSerializer(source="user_profile.job_title")
-    department = DepartmentSerializer(source="user_profile.department")
-    phone_number = serializers.IntegerField()
+    full_name_en = serializers.CharField(source="user_profile.full_name_en", default=None)
+    full_name_am = serializers.CharField(source="user_profile.full_name_am", default=None)
+    job_title = JobTitleSerializer(source="user_profile.job_title", default=None)
+    department = DepartmentSerializer(source="user_profile.department", default=None)
+    phone_number = serializers.IntegerField(default=None)
     email = serializers.EmailField()
 
 
 class CurrentUserSerializer(serializers.Serializer):
     id = serializers.UUIDField()
     email = serializers.EmailField()
-    first_name_en = serializers.CharField(source="user_profile.first_name_en")
-    middle_name_en = serializers.CharField(source="user_profile.middle_name_en")
-    last_name_en = serializers.CharField(source="user_profile.last_name_en")
-    first_name_am = serializers.CharField(source="user_profile.first_name_am")
-    middle_name_am = serializers.CharField(source="user_profile.middle_name_am")
-    last_name_am = serializers.CharField(source="user_profile.last_name_am")
-    job_title = JobTitleSerializer(source="user_profile.job_title")
-    department = DepartmentSerializer(source="user_profile.department")
-    phone_number = serializers.CharField(source="user_profile.phone_number")
+
+    first_name_en = serializers.CharField(source="user_profile.first_name_en", default=None)
+    middle_name_en = serializers.CharField(source="user_profile.middle_name_en", default=None)
+    last_name_en = serializers.CharField(source="user_profile.last_name_en", default=None)
+
+    first_name_am = serializers.CharField(source="user_profile.first_name_am", default=None)
+    middle_name_am = serializers.CharField(source="user_profile.middle_name_am", default=None)
+    last_name_am = serializers.CharField(source="user_profile.last_name_am", default=None)
+
+    job_title = JobTitleSerializer(source="user_profile.job_title", default=None)
+    department = DepartmentSerializer(source="user_profile.department", default=None)
+    phone_number = serializers.CharField(source="user_profile.phone_number", default=None)
+
     is_staff = serializers.BooleanField()
-    is_2fa_enabled = serializers.BooleanField(source="user_settings.is_2fa_enabled")
+    is_superuser = serializers.BooleanField()
+    is_2fa_enabled = serializers.BooleanField(source="user_settings.is_2fa_enabled", default=False)
