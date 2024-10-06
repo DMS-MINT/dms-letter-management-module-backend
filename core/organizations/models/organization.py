1f4bbe4640916cd87c393a4b85ec9f3eaf51d3d0
yab1 <yabilisanu@gmail.com>
2024-10-06 10:31:08 +0300

    refactor(organization): remove organization_directory_path function
    - removed the organization_directory_path function as it was unused.
    - ensured code cleanliness by eliminating unnecessary definitions.
    - maintained existing organization model functionality without change.

--- core/organizations/models/organization.py
+++ core/organizations/models/organization.py
@@ -6,10 +6,6 @@ from tenant_users.tenants.models import TenantBase
 from core.common.models import BaseModel
 
 
-def organization_directory_path(instance, filename):
-    return f"organizations/{instance.organization_name_en}/logo.png"
-
-
 class Organization(TenantBase, BaseModel):
     name_en = models.CharField(max_length=255, verbose_name=_("Organization Name (English)"))
     name_am = models.CharField(max_length=255, verbose_name=_("Organization Name (Amharic)"))
