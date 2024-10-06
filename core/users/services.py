b20c1ededc0e658cb3ff8aa8cc7a526c44764653
yab1 <yabilisanu@gmail.com>
2024-10-06 10:32:33 +0300

    feat(user): add user creation and update functions with transaction management
    - implemented user_create function to create a new user with job title and department.
    - added user_update function to modify existing user details with transaction management.
    - ensured proper type for phone_number and updated the corresponding fields in user profiles.
    
    BREAKING CHANGE: phone_number now accepts integers instead of strings in the user_create function.

--- core/users/services.py
+++ core/users/services.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from django.db import transaction
+
 from core.departments.models import Department, JobTitle
 
 from .models import User
@@ -16,8 +18,7 @@ def user_create(
     job_title: str,
     department: str,
     email: str,
-    phone_number: str,
-    is_active: bool = True,
+    phone_number: int,
     is_staff: bool = False,
     is_superuser: bool = False,
     password: Optional[str] = None,
@@ -37,9 +38,71 @@ def user_create(
             department=department_instance,
             email=email,
             phone_number=phone_number,
-            is_active=is_active,
+            is_active=True,
             is_staff=is_staff,
             is_superuser=is_superuser,
             password=password,
         ),
     )
+
+
+@transaction.atomic
+def user_update(
+    *,
+    user: User,
+    email: str = None,
+    first_name_en: str = None,
+    middle_name_en: str = None,
+    last_name_en: str = None,
+    first_name_am: str = None,
+    middle_name_am: str = None,
+    last_name_am: str = None,
+    job_title: str = None,
+    department: str = None,
+    phone_number: int = None,
+    is_staff: bool = None,
+    is_superuser: bool = None,
+) -> User:
+    if email is not None:
+        user.email = str(email)
+
+    if is_staff is not None:
+        user.is_staff = bool(is_staff)
+
+    if is_superuser is not None:
+        user.is_superuser = bool(is_superuser)
+
+    user.save()
+
+    user_profile = user.user_profile
+
+    if first_name_en is not None:
+        user_profile.first_name_en = str(first_name_en)
+
+    if middle_name_en is not None:
+        user_profile.middle_name_en = str(middle_name_en)
+
+    if last_name_en is not None:
+        user_profile.last_name_en = str(last_name_en)
+
+    if first_name_am is not None:
+        user_profile.first_name_am = str(first_name_am)
+
+    if middle_name_am is not None:
+        user_profile.middle_name_am = str(middle_name_am)
+
+    if last_name_am is not None:
+        user_profile.last_name_am = str(last_name_am)
+
+    if job_title is not None:
+        user_profile.job_title_id = str(job_title)
+
+    if department is not None:
+        user_profile.department_id = str(department)
+
+    if phone_number is not None:
+        user_profile.phone_number = int(phone_number)
+
+    user_profile.save()
+
+    return user
