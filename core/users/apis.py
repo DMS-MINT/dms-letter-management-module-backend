f8157aee5b9c9c0207770db8a4bf74507f8b2c4c
yab1 <yabilisanu@gmail.com>
2024-10-06 10:33:27 +0300

    feat(user): add user creation and update functionality
    - implemented user creation and update APIs with validation.
    - added detailed error handling for invalid inputs.
    - introduced a new UserCreateApi and UserUpdateApi classes for managing users.
    - improved error response consistency across user-related APIs.
    
    BREAKING CHANGE: updated error handling from NotFound to ValidationError for better clarity on input issues.

--- core/users/apis.py
+++ core/users/apis.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from rest_framework.exceptions import NotFound
+from rest_framework import status as http_status
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.api.mixins import ApiAuthMixin
+from core.common.utils import get_object
 from core.users.models import User
 
 from .selectors import user_get_users
 from .serializers import UserDetailSerializer, UserListSerializer
+from .services import user_create, user_update
 
 
 class UserListApi(ApiAuthMixin, APIView):
@@ -30,10 +33,11 @@ class UserListApi(ApiAuthMixin, APIView):
 
             return Response(data=response_data)
 
-        except NotFound as e:
-            raise NotFound({"message": str(e), "extra": {}}, status=404)
+        except ValueError as e:
+            raise ValidationError(e)
+
         except Exception as e:
-            return Response({"message": "An unexpected error occurred", "extra": {"details": str(e)}}, status=500)
+            raise ValidationError(e)
 
 
 class UserDetailAPI(ApiAuthMixin, APIView):
@@ -49,7 +53,108 @@ class UserDetailAPI(ApiAuthMixin, APIView):
 
             return Response(data=response_data)
 
-        except NotFound as e:
-            raise NotFound(e)
+        except ValueError as e:
+            raise ValidationError(e)
+
         except Exception as e:
-            return Response({"message": "An unexpected error occurred", "extra": {"details": str(e)}}, status=500)
+            raise ValidationError(e)
+
+
+class UserCreateApi(ApiAuthMixin, APIView):
+    class InputSerializer(serializers.Serializer):
+        email = serializers.EmailField()
+        password = serializers.CharField()
+
+        first_name_en = serializers.CharField()
+        middle_name_en = serializers.CharField()
+        last_name_en = serializers.CharField()
+
+        first_name_am = serializers.CharField()
+        middle_name_am = serializers.CharField()
+        last_name_am = serializers.CharField()
+
+        job_title = serializers.UUIDField()
+        department = serializers.UUIDField()
+        phone_number = serializers.IntegerField()
+
+        is_staff = serializers.BooleanField()
+        is_superuser = serializers.BooleanField()
+
+    def post(self, request):
+        input_serializer = self.InputSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+
+        try:
+            user_instance = user_create(**input_serializer.validated_data)
+
+            output_serializer = UserListSerializer(user_instance)
+
+            response_data = {"user": output_serializer.data}
+
+            return Response(data=response_data)
+
+        except ValueError as e:
+            raise ValidationError(e)
+
+        except Exception as e:
+            raise ValidationError(e)
+
+
+class UserUpdateApi(ApiAuthMixin, APIView):
+    class InputSerializer(serializers.Serializer):
+        email = serializers.EmailField(required=False)
+
+        first_name_en = serializers.CharField(required=False)
+        middle_name_en = serializers.CharField(required=False)
+        last_name_en = serializers.CharField(required=False)
+
+        first_name_am = serializers.CharField(required=False)
+        middle_name_am = serializers.CharField(required=False)
+        last_name_am = serializers.CharField(required=False)
+
+        job_title = serializers.UUIDField(required=False)
+        department = serializers.UUIDField(required=False)
+        phone_number = serializers.IntegerField(required=False)
+
+        is_staff = serializers.BooleanField(required=False)
+        is_superuser = serializers.BooleanField(required=False)
+
+    serializer_class = InputSerializer
+
+    def put(self, request, user_id):
+        user_instance = get_object(User, id=user_id)
+        input_serializer = self.serializer_class(data=request.data, partial=True)
+        input_serializer.is_valid(raise_exception=True)
+
+        try:
+            user_instance = user_update(user_instance=user_instance, **input_serializer.validated_data)
+
+            output_serializer = UserDetailSerializer(user_instance)
+
+            response_data = {"user": output_serializer.data}
+
+            return Response(data=response_data, status=http_status.HTTP_200_OK)
+
+        except ValueError as e:
+            raise ValidationError(e)
+
+        except Exception as e:
+            raise ValidationError(e)
+
+
+class UserDeleteApi(ApiAuthMixin, APIView):
+    def delete(self, request, user_id):
+        user_instance = get_object(User, id=user_id)
+
+        try:
+            user_instance.delete()
+
+            response_data = {"message": "User removed from your organization successfully."}
+
+            return Response(data=response_data, status=http_status.HTTP_200_OK)
+
+        except ValueError as e:
+            raise ValidationError(e)
+
+        except Exception as e:
+            raise ValidationError(e)
