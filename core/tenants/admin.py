3e0d84d3750bdabe1515ab837feba3a9e9a530bd
yab1 <yabilisanu@gmail.com>
2024-10-09 22:15:07 +0300

    feat: Add Tenant, TenantProfile, TenantSetting, and Domain models with admin configuration
    - Created models for Tenant, TenantProfile, TenantSetting, and Domain.
    - Implemented serializers and API views for tenant management.
    - Registered models in Django admin with custom admin classes:
      - TenantAdmin: Manage tenant details.
      - TenantProfileAdmin: Manage tenant profiles and contact information.
      - TenantSettingAdmin: Manage tenant-specific settings.
      - DomainAdmin: Manage tenant domains.
    
    This update facilitates the creation and management of tenants and their associated settings and profiles through the Django admin interface.

--- core/tenants/admin.py
+++ core/tenants/admin.py
@@ -1,3 +1,92 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Domain, Tenant, TenantProfile, TenantSetting
+
+
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ["name_en", "name_am"]
+    search_fields = ["name_en", "name_am"]
+
+    fieldsets = (
+        (
+            "Tenant Details",
+            {
+                "fields": ("name_en", "name_am"),
+            },
+        ),
+    )
+
+    show_in_index = True
+
+
+@admin.register(TenantProfile)
+class TenantProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        "tenant",
+        "contact_phone",
+        "contact_email",
+        "postal_code",
+        "address",
+    ]
+    search_fields = [
+        "contact_phone",
+        "contact_email",
+        "postal_code",
+    ]
+
+    fieldsets = (
+        (
+            "Tenant Profile Details",
+            {
+                "fields": (
+                    "tenant",
+                    "description",
+                    "contact_phone",
+                    "contact_email",
+                    "address",
+                    "postal_code",
+                    "logo",
+                ),
+            },
+        ),
+    )
+
+    readonly_fields = ["logo"]
+
+    show_in_index = True
+
+
+@admin.register(TenantSetting)
+class TenantSettingAdmin(admin.ModelAdmin):
+    list_display = ["tenant", "auto_ref_number_letters"]
+    fieldsets = (
+        (
+            "Settings",
+            {
+                "fields": (
+                    "tenant",
+                    "auto_ref_number_letters",
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ["domain", "tenant"]
+    search_fields = [
+        "domain",
+    ]
+
+    fieldsets = (
+        (
+            "Domain Details",
+            {
+                "fields": ("domain", "tenant", "is_primary"),
+            },
+        ),
+    )
+
+    show_in_index = True
---
+++ core/tenants/models/__init__.py
@@ -0,0 +1,4 @@
+from .tenants import Tenant as Tenant  # noqa: I001
+from .tenants import Domain as Domain
+from .profile import TenantProfile as TenantProfile
+from .settings import TenantSetting as TenantSetting
