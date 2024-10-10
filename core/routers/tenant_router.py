5adba46a5865742c9586644c4ea0c151ae1585bb
yab1 <yabilisanu@gmail.com>
2024-10-10 11:41:22 +0300

    refactor(router): update db_for_read and db_for_write methods to use get_current_db_name
    - modified the db_for_read method to return the current database name using get_current_db_name.
    - updated the db_for_write method similarly for consistency in database operations.
    - allowed relations to be determined without direct reference to the database name.
    
    BREAKING CHANGE: the database routing logic now relies on get_current_db_name for tenant apps.

--- core/routers/tenant_router.py
+++ core/routers/tenant_router.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from core.tenants.middleware import get_current_db_name
+
 
 class TenantRouter:
     tenant_local_apps_labels = {app_config_path.split(".")[1] for app_config_path in settings.TENANT_LOCAL_APPS}
@@ -10,7 +12,7 @@ class TenantRouter:
             model._meta.app_label in settings.TENANT_THIRD_PARTY_APPS
             or model._meta.app_label in self.tenant_local_apps_labels
         ):
-            return "tenant"
+            return get_current_db_name()
         return None
 
     def db_for_write(self, model, **hints):
@@ -19,7 +21,7 @@ class TenantRouter:
             model._meta.app_label in settings.TENANT_THIRD_PARTY_APPS
             or model._meta.app_label in self.tenant_local_apps_labels
         ):
-            return "tenant"
+            return get_current_db_name()
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
@@ -27,8 +29,6 @@ class TenantRouter:
         if (
             obj1._meta.app_label in self.tenant_local_apps_labels
             or obj2._meta.app_label in self.tenant_local_apps_labels
-            or obj1._meta.app_label == "users"
-            or obj2._meta.app_label == "user_management"
         ):
             return True
         return False
@@ -36,5 +36,5 @@ class TenantRouter:
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """Allow migrations for tenant apps in the tenant db."""
         if app_label in settings.TENANT_THIRD_PARTY_APPS or app_label in self.tenant_local_apps_labels:
-            return db == "tenant"
+            return db == get_current_db_name()
         return False
