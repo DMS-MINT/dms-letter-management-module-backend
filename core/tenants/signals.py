d7c9a3c3c9c8eff72ede4dfb019c6bbb5f9b7d66
yab1 <yabilisanu@gmail.com>
2024-10-10 11:53:07 +0300

    feat(tenant): add signal to create tenant database and set owner permissions
    - implemented a signal receiver to create a tenant database upon tenant creation.
    - added functionality to set permissions for the owner of the newly created tenant.
    - ensured proper database migrations are triggered for the new tenant database.

---
+++ core/tenants/signals.py
@@ -0,0 +1,34 @@
+from django.core.management import call_command
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from core.user_management.service import add_user
+from core.users.models import User
+
+from .middleware import set_db_for_router
+from .models import Tenant
+
+
+def set_owner_permissions(current_user: User, tenant_instance: Tenant):
+    add_user(
+        current_user=current_user,
+        tenant_instance=tenant_instance,
+        is_admin=True,
+        is_staff=True,
+        is_superuser=True,
+    )
+
+
+@receiver(post_save, sender=Tenant)
+def create_tenant_database(sender, instance, created, **kwargs):
+    if created:
+        # tenant_database_name = instance.database_name
+        tenant_database_name = "tenant"
+        try:
+            call_command("migrate", database=tenant_database_name)
+        except Exception as e:
+            raise ValueError(e)
+
+        set_db_for_router(tenant_database_name)
+
+        set_owner_permissions(current_user=instance.owner, tenant_instance=instance)
